58fe3085929e9779e6f9da1b496cb51c2cc807db
disappearwind <disappearwind@f5c38212-3326-11de-9c63-4752e237b823>
2009-04-27 14:09:35 +0000

    add fetion service

---
+++ trunk/MobileService/trunk/UnitTest/FetionMainServiceTest.cs
@@ -0,0 +1,87 @@
+﻿using Disappearwind.MobileService.FetionService;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+namespace Disappearwind.MobileService.UnitTest
+{
+
+
+    /// <summary>
+    ///This is a test class for FetionMainServiceTest and is intended
+    ///to contain all FetionMainServiceTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class FetionMainServiceTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for SendMessage
+        ///</summary>
+        [TestMethod()]
+        public void SendMessageTest()
+        {
+            long sendNumber = 13488794972;
+            string password = "disappearwind";
+            long reviceNumber = 13488794972;
+            string content = "test";
+            try
+            {
+                FetionMainService.SendMessage(sendNumber, password, reviceNumber, content);
+                Assert.IsTrue(true);
+            }
+            catch (Exception ex)
+            {
+                Assert.Inconclusive(ex.Message);
+            }
+        }
+    }
+}
---
+++ trunk/MobileService/trunk/Utility/WebAccess.cs
@@ -0,0 +1,53 @@
+﻿/****************************************************************************
+ * Copyright (C) Disappearwind. All rights reserved.                        *
+ *                                                                          *
+ * Author: disapearwind                                                     *
+ * Created: 2009-4-27                                                       *
+ *                                                                          *
+ * Description:                                                             *
+ *   Access web.Copy from blogsolution,but less params                      *
+ *                                                                          *
+*****************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace Disappearwind.MobileService.Utility
+{
+    /// <summary>
+    /// Access web url
+    /// </summary>
+    public class WebAccess
+    {
+        /// <summary>
+        /// Request a url with data and return the url's response
+        /// </summary>
+        /// <param name="url">The request url</param>
+        /// <returns>return response</returns>
+        public static string GetRequest(string url)
+        {
+            try
+            {
+                string result = string.Empty;
+                //request
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                req.Method = "Get";
+                //get response
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                StreamReader sr = new StreamReader(res.GetResponseStream(), System.Text.Encoding.UTF8);
+                result = sr.ReadToEnd();
+                sr.Close();
+                res.Close();
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
