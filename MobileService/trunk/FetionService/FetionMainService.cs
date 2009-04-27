90786c5ca50626f0e58f162da4135acb740ded32
disappearwind <disappearwind@f5c38212-3326-11de-9c63-4752e237b823>
2009-04-27 14:09:35 +0000

    add fetion service

---
+++ MobileService/trunk/FetionService/FetionMainService.cs
@@ -0,0 +1,43 @@
+﻿/****************************************************************************
+ * Copyright (C) Disappearwind. All rights reserved.                        *
+ *                                                                          *
+ * Author: disapearwind                                                     *
+ * Created: 2009-4-27                                                       *
+ *                                                                          *
+ * Description:                                                             *
+ *   Fetion main service. call some fetion api provided by sms              *
+ *   api:http://sms.api.bz/                                                 *
+ *                                                                          *
+*****************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Disappearwind.MobileService.FetionService
+{
+    /// <summary>
+    /// Fetion main service
+    /// </summary>
+    public class FetionMainService
+    {
+        /// <summary>
+        /// The api http address
+        /// </summary>
+        public static readonly string FetionAPI = "http://sms.api.bz/";
+
+        /// <summary>
+        /// Use fetion send message to a mobile
+        /// </summary>
+        /// <param name="sendNumber">sender mobile number</param>
+        /// <param name="password">password for sender to login</param>
+        /// <param name="reviceNumber">message revicer's mobile number</param>
+        /// <param name="content">message content</param>
+        public static void SendMessage(long sendNumber, string password, long reviceNumber, string content)
+        {
+            string requestURL = string.Format("http://sms.api.bz/fetion.php?username={0}&password={1}&sendto={2}&message={3}", 
+                sendNumber, password, reviceNumber, content);
+            Disappearwind.MobileService.Utility.WebAccess.GetRequest(requestURL);
+        }
+    }
+}
---
+++ MobileService/trunk/FetionService/UserInfo.cs
@@ -0,0 +1,36 @@
+﻿/****************************************************************************
+ * Copyright (C) Disappearwind. All rights reserved.                        *
+ *                                                                          *
+ * Author: disapearwind                                                     *
+ * Created: 2009-4-27                                                       *
+ *                                                                          *
+ * Description:                                                             *
+ *   Fetion user information                                                *
+ *                                                                          *
+*****************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Disappearwind.MobileService.FetionService
+{
+    /// <summary>
+    /// User info of fetion
+    /// </summary>
+    public class UserInfo
+    {
+        /// <summary>
+        /// Mobile number
+        /// </summary>
+        public long Number { get; set; }
+        /// <summary>
+        /// Display name of user
+        /// </summary>
+        public string DisplayName { get; set; }
+        /// <summary>
+        /// Password to login fetion
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
